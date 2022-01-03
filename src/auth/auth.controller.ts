3f4329223a1d068a1df31c879fabaa1fa8b5b0de
Eva <it211506@fhstp.ac.at>
2022-01-03 11:38:17 +0100

    added login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/Login.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('/login')
   protected async login(
     @Body(ValidationPipe) loginDto: LoginDto,
   ): Promise<any> {
-    return 'login';
+    // return 'login';
+    return this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dtos/Login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  public async login(loginDto: LoginDto): Promise<any> {
+    return loginDto;
+  }
+}
